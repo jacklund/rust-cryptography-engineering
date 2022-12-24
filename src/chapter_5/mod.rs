b78de799df8848c4755d6aa7011966fb7adad965
Jack Lund <jackl@geekheads.net>
2022-12-24 00:35:47 -0600

    Add exercise 5.4

--- src/chapter_5/mod.rs
+++ src/chapter_5/mod.rs
@@ -1,4 +1,5 @@
 use super::sha512_n;
+use hex::decode;
 use std::collections::HashMap;
 use std::time::{Duration, Instant};
 
@@ -26,5 +27,44 @@ pub fn exercise_5_3(bytes: usize, times: usize) {
             .iter()
             .sum::<Duration>()
             .div_f32(timings.len() as f32)
+    );
+}
+
+pub fn exercise_5_4(bytes: usize, message: &str, times: usize) {
+    let msg = match decode(message) {
+        Ok(msg) => {
+            if msg.len() != bytes {
+                eprintln!(
+                    "Expected message with {} bytes, got {} bytes",
+                    bytes,
+                    msg.len()
+                );
+                return;
+            }
+            msg
+        }
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            return;
+        }
+    };
+    let mut runs = vec![];
+    for _ in 1..times {
+        let iterations = (1usize..100_000_000)
+            .find_map(|i| {
+                let hash = sha512_n(&i.to_be_bytes(), bytes);
+                if hash == msg {
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+            .unwrap();
+        runs.push(iterations);
+    }
+
+    println!(
+        "Average number of iterations: {}",
+        runs.iter().sum::<usize>() as f32 / runs.len() as f32
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::chapter_5::exercise_5_3;
+use crate::chapter_5::*;
 use clap::{Parser, Subcommand};
 use ring::digest;
 
@@ -8,10 +8,17 @@ fn sha512_n(value: &[u8], bytes: usize) -> Vec<u8> {
     digest::digest(&digest::SHA512, value).as_ref()[..bytes].into()
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Subcommand)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Subcommand)]
 enum Exercises {
     #[command(name = "5.3")]
     E5_3 { bytes: usize, times: usize },
+
+    #[command(name = "5.4")]
+    E5_4 {
+        bytes: usize,
+        message: String,
+        times: usize,
+    },
 }
 
 #[derive(Parser, Debug)]
@@ -25,5 +32,10 @@ fn main() {
 
     match args.exercise {
         Exercises::E5_3 { bytes, times } => exercise_5_3(bytes, times),
+        Exercises::E5_4 {
+            bytes,
+            message,
+            times,
+        } => exercise_5_4(bytes, &message, times),
     }
 }
